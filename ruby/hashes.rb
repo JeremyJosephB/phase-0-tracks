63d6850caf3a6549b14f23007bf8dadfaa6a14a0
JeremyJosephB <jjbb2698@msn.com>
2016-11-04 15:21:21 -0400

    Created data structure and user interface

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -6,3 +6,28 @@
 #    would be good
 # 4. print back the hash.
 
+client_app = {
+	name: "",
+	age: 0,
+	number_of_kids: 0,
+	decor_theme: "",
+	using_credit: true
+}
+
+puts "What's the client's name?"
+	client_app[:name] = gets.chomp
+puts "What's the client's age?"
+	client_app[:age] = gets.chomp.to_i
+puts "How kids does the client have?"
+	client_app[:number_of_kids] = gets.chomp.to_i
+puts "What decor theme does the client want?"
+	client_app[:decor_theme] = gets.chomp
+puts "What is the cilent using credit?"
+	using_credit = gets.chomp
+	case using_credit
+		when "true" then client_app[:using_credit] = true
+		when "false" then client_app[:using_credit] = false
+	end
+
+print client_app
+		
