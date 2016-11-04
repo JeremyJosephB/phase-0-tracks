6a46acacaa7c6e04e5bdf5f7e23bd77fd7647109
JeremyJosephB <jjbb2698@msn.com>
2016-11-04 14:27:53 -0400

    initial project and pseudocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,8 @@
+# Pseudocode: 
+# 1. create the data structure in order to hold any data to begin with.
+# 2. In the driver code get user input, it's to be able to take anything 
+#    so gets.chomp should should work
+# 3. store that data in the data structure, "hash[0] = user_input"
+#    would be good
+# 4. print back the hash.
+
