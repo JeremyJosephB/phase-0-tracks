8ccc2faee9936671c647739ac24b00a727d70abb
JeremyJosephB <jjbb2698@msn.com>
2016-11-06 20:03:02 -0500

    Completed release 2

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -29,3 +29,35 @@ index = 2
 end
 
 fib(100)
+
+# pseudocode for insertion sort algorithm
+# 1. look at first index
+# 2. that one index will be the first sorted index
+# 3. the next index in the unsorted array will be compared with
+#    indexes of the sorted array.
+# 4. the condition IF the unsorted element is greater than the sorted element
+#    it will be put after the sorted element.
+# 5. OTHERWISE the unsorted element will be placed before the sorted element
+# 6. continue until all elements are sorted.
+
+def insertion_sort(array)
+    final = [array[0]]
+    array.delete_at(0)
+    for i in array
+        final_index = 0
+        while final_index < final.length
+            if i <= final[final_index]
+                final.insert(final_index,i)
+                break
+            elsif final_index == final.length-1
+                final.insert(final_index+1,i)
+                break
+            end
+            final_index+=1
+        end
+    end
+    
+    final
+end
+array = [1, 5, 3, 4, 6, 3]
+puts insertion_sort(array)
