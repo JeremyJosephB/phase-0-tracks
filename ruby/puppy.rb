8a1253b17250f1a8a941b140bb7abcf4a38d82d9
JeremyJosephB <jjbb2689@msn.com>
2016-11-08 19:21:59 -0500

    already understand this will hand in old homework

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+class Puppy
+end
+
+nina = Puppy.new
+
+blackie = Puppy.new
+
+chikitin = Puppy.new
+
+tank = "ugly doggo"
+
+p tank.length #should be nine unless the space sounts then it's ten
+
+p nina.methods
+
+p blackie.instance_of?(Puppy)
