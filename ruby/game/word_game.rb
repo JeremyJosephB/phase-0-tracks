af2f570594b6baf4fe9050578c7e6c2e4130e60e
JeremyJosephB <jjbb2689@msn.com>
2016-11-14 10:37:26 -0500

    realized I was ahead of myself when I coding for did_player_win. I'm now writing code for checking the guesses

--- ruby/game/word_game.rb
+++ ruby/game/word_game.rb
@@ -7,16 +7,23 @@ class WordGame
 		@guessed_word = []
 		@guess_count = word.length
 	    @is_over = false
-	    puts "Great! let the guessing game, Begin!"
-	    @correct_word.chars.each_with_index {|letter,index| @guessed_word[index] = "?"}
+	    p "Great! let the guessing game, Begin!"
+	    @correct_word.chars.each_index {|index| @guessed_word[index] = "?"}
+	    
 	end
 	
 	def check_guess(guessed_letter)
+		
+	end
+	
+	def did_player_win
 		@guess_count -= 1
-		if @guess_count == 0 && guessed_letter 
+		if @guess_count == 0 && @guessed_word != @word
 			@is_over = true
-		elsif guessed_letter == @word
+			puts "Sorry you lose, maybe next time."
+		elsif @guessed_word.join("") == @word
 			@is_over = true
+			puts
 		else
 			false
 		end
@@ -27,5 +34,17 @@ puts "Welcome to our word-guessing game!"
 puts "Please input a word for someone to guess."
 
 word = gets.chomp
-
 game = WordGame.new(word)
+
+puts "Make a guess!"
+
+guessed_letter = gets.chomp
+game.check_guess(guessed_letter)
+
+while !game.is_over
+	p @guessed_word
+	puts "Make a guess!"
+	guessed_letter = gets.chomp
+	game.check_guess(guessed_letter)
+	game.did_player_win
+end
