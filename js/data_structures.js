a71d2249aa0a2c2d6c5963ed729e08350283fa77
JeremyJosephB <jjbb2689@msn.com>
2016-11-17 16:58:15 -0500

    did arrays functions

---
+++ js/data_structures.js
@@ -0,0 +1,9 @@
+colors = ["blue", "red", "rainbow", "orange"];
+names = ["Ed", "Bill", "Bob", "Chuck"];
+
+function add_stuff(array, item) {
+	array.push(item);
+	console.log(array);
+}
+add_stuff(colors,"pink")
+add_stuff(names,"floyd")
