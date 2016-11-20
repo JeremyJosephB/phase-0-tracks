5bdae0c6804c30a95e7d97ef812a29f76483e8c3
JeremyJosephB <jjbb2689@msn.com>
2016-11-20 15:49:59 -0500

    completed string gen function

--- js/algos.js
+++ js/algos.js
@@ -1,14 +1,38 @@
-testing = ["long phrase","longest phrase","longer phrase"];
 function whosBigger(arr) {
 	console.log(arr);
 	var lgth = 0;
 	var longest;
 	for(var i=0; i < arr.length; i++){
 	    if(arr[i].length > lgth){
-	        var lgth = arr[i].length;
+	        lgth = arr[i].length;
 	        longest = arr[i];
 	    }      
 	} 
 	return console.log(longest);
 }
+
+// I didn't know how to do it in one function so I seperated the word generating part
+// into a seperate function and called it in the string generator
+function wordgen() {
+	var text = "";
+    var charset = "abcdefghijklmnopqrstuvwxyz0123456789";
+	var lngth = (Math.random() * 10) + 1;
+    for(var i=0; i < lngth; i++){
+    	text += charset.charAt(Math.floor(Math.random() * charset.length));
+	}
+	return text;
+}
+
+function stringGen(words){
+	var array = [];
+	for(var i=0; i < words; i++){
+	    array.push(wordgen());
+	}
+    return array;
+}
+
+var testing = ["long phrase","longest phrase","longer phrase"];
+var testing2 = stringGen(10);
+
+whosBigger(testing2);
 whosBigger(testing);
