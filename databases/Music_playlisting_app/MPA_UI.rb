5b4d95e5f9620313e6035ecdff76bd7cc7488d23
JeremyJosephB <jjbb2689@msn.com>
2016-11-28 01:47:42 -0500

    added exit option

--- databases/Music_playlisting_app/MPA_UI.rb
+++ databases/Music_playlisting_app/MPA_UI.rb
@@ -4,7 +4,7 @@ database = SQLite3::Database.new("MUsic_playlist.db")
 
 
 # loop do 
-# 	puts "Do you want to create a playlist, or are you deleting or updating one? (Type 'new', 'update', or 'delete')"
+# 	puts "Do you want to create a playlist, or are you deleting or updating one? (Type 'new', 'update', 'delete', or 'exit' to exit the program)"
 # 	user_action = gets.chomp
 	
 # end
