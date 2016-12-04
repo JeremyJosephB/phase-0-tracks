64a93949c0bb022e10f5cec38a7e926784c09c30
JeremyJosephB <jjbb2689@msn.com>
2016-12-04 14:07:56 -0500

    practiced with jquery

--- web_dev/gondola_site_v2/script.js
+++ web_dev/gondola_site_v2/script.js
@@ -1,10 +1,14 @@
 console.log("The script is running!");
 
-function addBlackBorder(event) {
-  console.log("click happened!");
-  console.log(event);
-  event.target.style.border = "3px solid black";
-}
-
-var video = document.getElementById("gondola-webm");
-video.addEventListener("click", addBlackBorder);
+$(document).ready(function (){
+  $("button#enlighten").click(function(){
+    $("video#gondola-webm").animate({height:"toggle", width:"toggle"},"slow");
+  });
+  $("button#Hide").click(function(){
+    ('#content').hide();
+    $('#Show').appendTo('body');
+  });
+  $("button#Show").click(function(){
+    $("*").show();
+  });
+});
