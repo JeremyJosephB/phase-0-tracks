aa68a8ce04fd5b05dc81ade0d888526bfbb7c7cc
JeremyJosephB <jjbb2689@msn.com>
2016-12-04 16:45:14 -0500

    completed everything minus the bonus question

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -1,5 +1,6 @@
 # require gems
 require 'sinatra'
+require "sinatra/reloader" if development?
 require 'sqlite3'
 
 db = SQLite3::Database.new("students.db")
@@ -43,4 +44,27 @@ end
 get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
+end
+
+get '/contact' do
+  "<h1>3086 Canis Heights Drive <br>
+  Los Angeles <br>
+  California <br>
+  90057 <br>
+  213-639-1386 <br></h1>"
+end
+
+get '/great_job' do
+  name = params[:name]
+  if name
+    "<h2>Good Job, #{name}!</h2>"
+  else
+    "<h2>Good Job!</h2>"
+  end
+end
+get '/math/:number1/plus/:number2' do
+  number1 = params[:number1]
+  number2 = params[:number2]
+  result = number1.to_i + number2.to_i
+  "<h1>#{number1} + #{number2} = #{result}</h1>"
 end
