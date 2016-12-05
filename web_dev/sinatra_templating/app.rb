ca8aad3f777a2980786aa55a30b6daef972c686b
JeremyJosephB <jjbb2689@msn.com>
2016-12-04 19:12:51 -0500

    add new car form

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -10,6 +10,7 @@ db.results_as_hash = true
 # show students on the home page
 get '/' do
   @students = db.execute("SELECT * FROM students")
+  @cars = db.execute("SELECT * FROM cars")
   erb :home
 end
 
@@ -17,6 +18,9 @@ get '/students/new' do
   erb :new_student
 end
 
+get '/cars/new' do
+  erb :new_car
+end
 # create new students via
 # a form
 post '/students' do
@@ -24,4 +28,8 @@ post '/students' do
   redirect '/'
 end
 
+post '/cars' do
+  db.execute("INSERT INTO cars (car_brand) VALUES (?)", [params[:car_brand]])
+  redirect '/'
+end
 # add static resources
